dd3390703fbb76ced30217ecbe96a282f859f977
deLears <91895629+deLears@users.noreply.github.com>
2021-10-09 16:31:55 +0300

    Add files via upload

---
+++ Podzadacha2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Podzadacha2
+    {
+        static void Main(string[] args)
+        {
+            // Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+            Console.WriteLine("Введите сторону куба:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double S, V;
+            Cube(a, out S, out V);
+            Console.WriteLine("Площадь поверхности куба:{0}",S);
+            Console.WriteLine("Объем куба:{0}", V);
+            Console.ReadKey();
+        }
+        static void Cube(double a, out double S, out double V)
+        {
+            S = 6 * (a*a);
+            V = Math.Pow(a,3);
+            
+        }
+    }
+}
